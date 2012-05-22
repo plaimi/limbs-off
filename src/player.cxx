c402478ff4c93caa75219527e0a59ae5061ce66f
Alexander Berntsen <alexanderberntsen@linux.com>
2012-05-22 19:35:56 +0200

    Update wrong email address in player.cxx

--- src/player.cxx
+++ src/player.cxx
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Alexander Berntsen <alexanderberntsen@plaimi.net>
+ * Copyright (C) 2011 Alexander Berntsen <alexander@plaimi.net>
  *
  * This file is part of Limbs Off.
  *
